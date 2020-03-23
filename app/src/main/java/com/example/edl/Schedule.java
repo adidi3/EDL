63c43a66731bd2e82e92bf4055add347161e4c33
adidi3 <ap0413@bs.amalnet.k12.il>
2020-03-23 19:06:45 +0200

    with list view and time picker

---
+++ app/src/main/java/com/example/edl/Schedule.java
@@ -0,0 +1,59 @@
+package com.example.edl;
+
+public class Schedule {
+    private String sunday, monday, tuesday, wednesday, thursday, friday;
+    public Schedule (String sunday, String monday, String tuesday, String wednesday, String thursday, String friday) {
+        this.sunday=sunday;
+        this.monday=monday;
+        this.tuesday=tuesday;
+        this.wednesday=wednesday;
+        this.thursday=thursday;
+        this.friday=friday;
+
+    }
+    public String getSunday() {
+        return sunday;
+    }
+
+    public void setSunday(String sunday) {
+        this.sunday=sunday;
+    }
+
+    public String getMonday() {
+        return monday;
+    }
+
+    public void setMonday(String monday) {
+        this.monday=monday;
+    }
+
+    public String getTuesday() { return tuesday; }
+
+    public void setTuesday(String tuesday) {
+        this.tuesday=tuesday;
+    }
+
+    public String getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(String wednesday) {
+        this.wednesday=wednesday;
+    }
+
+    public String getThursday() {
+        return thursday;
+    }
+
+    public void setThursday(String thursday) {
+        this.thursday=thursday;
+    }
+
+    public String getFriday() {
+        return friday;
+    }
+
+    public void setFriday(String friday) {
+        this.friday=friday;
+    }
+}
--- app/src/main/java/com/example/edl/lessonsTeachers.java
+++ app/src/main/java/com/example/edl/lessonsTeachers.java
@@ -1,14 +1,76 @@
 package com.example.edl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class lessonsTeachers extends AppCompatActivity {
+
+    TextView tvTime;
+    Calendar ca;
+    int Nhour, Nminute;
+    String fo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons_teachers);
+        tvTime= (TextView) findViewById(R.id.tvTime);
+
+        ca= Calendar.getInstance();
+
+        Nhour=ca.get(Calendar.HOUR_OF_DAY);
+        Nminute=ca.get(Calendar.MINUTE);
+
+        sTF(Nhour);
+        tvTime.setText(Nhour+ " : "+Nminute+ " "+fo );
+        tvTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog= new TimePickerDialog(lessonsTeachers.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int Nhour, int Nminute) {
+                        sTF(Nhour);
+                        tvTime.setText(Nhour+ " : "+Nminute+ " "+fo );
+                    }
+                }, Nhour, Nminute, true);
+                timePickerDialog.show();;
+            }
+        });
+
+    }
+    public void sTF(int Nhour){
+        if (Nhour==0) {
+            Nhour += 12;
+            fo = "AM";
+        }
+        else if (Nhour==12) {
+            fo = "PM";
+        }
+        else if (Nhour>12){
+            fo="PM";
+        }
+        else {
+            fo="AM";
+        }
     }
 }
+
+
+
+
+
+
