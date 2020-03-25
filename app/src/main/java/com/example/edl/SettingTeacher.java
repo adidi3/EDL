e5efd03898b33c0a7888f61d8289e49d3faa56c6
adidi3 <ap0413@bs.amalnet.k12.il>
2020-03-25 17:00:18 +0200

    with list view and time picker- setting without FB with menu

--- app/src/main/java/com/example/edl/SettingTeacher.java
+++ app/src/main/java/com/example/edl/SettingTeacher.java
@@ -3,27 +3,30 @@ package com.example.edl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class SettingTeacher extends AppCompatActivity {
 
-    TextView tvTimeStart, tvTimeEnd;
+    TextView tvTimeStart, tvTimeEnd, tvDays;
     Calendar ca;
-    int Nhour, Nminute;
+    int Nhour, Nminute, dayCount=1;
     String fo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_teacher);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lessons_teachers);
         tvTimeStart= (TextView) findViewById(R.id.tvTimeStart);
+        tvDays= (TextView) findViewById(R.id.textView2);
         tvTimeEnd= (TextView) findViewById(R.id.tvTimeEnd);
 
         ca= Calendar.getInstance();
@@ -47,6 +50,21 @@ public class SettingTeacher extends AppCompatActivity {
                 timePickerDialog.show();;
             }
         });
+        tvTimeEnd.setText("when do you end your day?");
+        tvTimeEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog= new TimePickerDialog(SettingTeacher.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int Nhour, int Nminute) {
+
+                        sTF(Nhour);
+                        tvTimeEnd.setText(Nhour+ " : "+Nminute+ " "+fo );
+                    }
+                }, Nhour, Nminute, true);
+                timePickerDialog.show();;
+            }
+        });
 
     }
     public void sTF(int Nhour){
@@ -65,6 +83,7 @@ public class SettingTeacher extends AppCompatActivity {
         }
     }
 
+
   /*  Nminute= Nminute+40;
                         if (Nminute>=60){
         Nhour=Nhour+1;
@@ -75,12 +94,77 @@ public class SettingTeacher extends AppCompatActivity {
     }
 **/
     public void bl(View view) {
+        if (dayCount>1){
+            dayCount--;
+            switch (dayCount){
+                case 1: tvDays.setText("Sunday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");
+                break;
+                case 2: tvDays.setText("Monday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 3: tvDays.setText("Tuesday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 4: tvDays.setText("Wednesday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 5: tvDays.setText("Thursday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+            }
+        }
+        Toast.makeText(SettingTeacher.this, "it's the first day!", Toast.LENGTH_LONG).show();
     }
 
     public void br(View view) {
+        if (dayCount<6){
+            dayCount++;
+            switch (dayCount){
+                case 2: tvDays.setText("Monday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 3: tvDays.setText("Tuesday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 4: tvDays.setText("Wednesday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 5: tvDays.setText("Thursday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                case 6: tvDays.setText("Friday");
+                    tvTimeEnd.setText("when do you end your day?");
+                    tvTimeStart.setText("when do you start your day?");break;
+                }
+        }
+        Toast.makeText(SettingTeacher.this, "it's the last day!", Toast.LENGTH_LONG).show();
     }
 
     public void Et(View view) {
+        switch (dayCount){
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4:break;
+            case 5: break;
+            case 6: ;break;
+        }
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //menu
+        String st=item.getTitle().toString();
+        if (st.equals("Home screen")){
+            Intent gotogal = new Intent(this, lessonsTeachers.class);
+            startActivity(gotogal);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
--- app/src/main/java/com/example/edl/lessonsTeachers.java
+++ app/src/main/java/com/example/edl/lessonsTeachers.java
@@ -5,9 +5,12 @@ import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -19,10 +22,27 @@ import java.util.Calendar;
 public class lessonsTeachers extends AppCompatActivity {
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //menu
+        String st=item.getTitle().toString();
+
+        if (st.equals("Setting")) {
+            Intent gotogal = new Intent(this, SettingTeacher.class);
+            startActivity(gotogal);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
