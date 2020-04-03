c9cbb59c57760d8d1392036949a5102440897786
adidi3 <ap0413@bs.amalnet.k12.il>
2020-04-03 05:26:46 +0300

    schesule done.2

--- app/src/main/java/com/example/edl/TeacherLessons.java
+++ app/src/main/java/com/example/edl/TeacherLessons.java
@@ -44,14 +44,14 @@ public class TeacherLessons extends AppCompatActivity implements AdapterView.OnI
     Day day1= new Day();
     Week week1=new Week();
     String phonestudent="", phonestudent1;
-    AlertDialog.Builder ad;
+    AlertDialog.Builder ad, adb;
     Ustudents student = new Ustudents();
     TextView vname;
-    String count1="";
-    LinearLayout dialog;
+    String count1;
+    LinearLayout dialog, dialogex;
     Uteachers user;
     int t;
-    String uid;
+    String uid, tmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -238,62 +238,121 @@ public class TeacherLessons extends AppCompatActivity implements AdapterView.OnI
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, final int position, long l) {
-        dialog = (LinearLayout) getLayoutInflater().inflate(R.layout.dialogx, null);
-        ad = new AlertDialog.Builder(this);
-        ad.setCancelable(false);
-        ad.setTitle("Confirm deleting value from Firebase");
-        ad.setView(dialog);
-        ad.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                String str=("l"+position);
-                phonestudent1 = stringList.get(position);
-                if((!phonestudent1.equals("08:00-08:40"))&&!phonestudent1.equals("08:45-09:25")&&!phonestudent1.equals("10:00-10:40")&&!phonestudent1.equals("10:45-11:25")&&!phonestudent1.equals("12:30-13:10")&&!phonestudent1.equals("13:15-13:55")&&
-                        !phonestudent1.equals("14:00-14:40")&&!phonestudent1.equals("14:45-15:25")&&!phonestudent1.equals("19:00-19:40")&&!phonestudent1.equals("19:45-20:25")) {
+        phonestudent1 = stringList.get(position);
+        if (phonestudent1.equals("Canceled")) {
+            dialogex = (LinearLayout) getLayoutInflater().inflate(R.layout.dialogxxx, null);
+            adb = new AlertDialog.Builder(this);
+            adb.setCancelable(false);
+            adb.setTitle("would you like to return this lesson?");
+            adb.setView(dialogex);
+            adb.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    str = ("l" + (position));
 
-
-                for (int x = 0; x <= 9; x++)
-                    phonestudent =phonestudent+phonestudent1.charAt(x);
-                refStudent.child(phonestudent).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        student.copyStudent(dataSnapshot.getValue(Ustudents.class));
-                        count1 = student.getCount();
-                        t = Integer.parseInt(count1);
-                        t--;
-                        String sr = "count";
-                        String sc = Integer.toString(t);
-                        refStudent.child(phonestudent).child(sr).removeValue();
-                        refStudent.child(phonestudent).child(sr).setValue(sc);
-                        Toast.makeText(TeacherLessons.this, "Deleting succeeded", Toast.LENGTH_SHORT).show();
-                        phonestudent = "";
-
-
+                    switch (position) {
+                        case 0:
+                            tmp = "08:00-08:40";
+                            break;
+                        case 1:
+                            tmp = "08:45-09:25";
+                            break;
+                        case 2:
+                            tmp = "10:00-10:40";
+                            break;
+                        case 3:
+                            tmp = "10:45-11:25";
+                            break;
+                        case 4:
+                            tmp = "12:30-13:10";
+                            break;
+                        case 5:
+                            tmp = "13:15-13:55";
+                            break;
+                        case 6:
+                            tmp = "14:00-14:40";
+                            break;
+                        case 7:
+                            tmp = "14:45-15:25";
+                            break;
+                        case 8:
+                            tmp = "19:00-19:40";
+                            break;
+                        case 9:
+                            tmp = "19:45-20:25";
+                            break;
                     }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                });
+
+                    refTeacherTime.child(phone1).child(day).child(str).removeValue();
+                    refTeacherTime.child(phone1).child(day).child(str).setValue(tmp);
+                    Toast.makeText(TeacherLessons.this, "succeeded", Toast.LENGTH_SHORT).show();
+                    dialogInterface.dismiss();
                 }
+
+            });
+            adb.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            AlertDialog adb1 = adb.create();
+            adb1.show();
+        } else {
+
+            dialog = (LinearLayout) getLayoutInflater().inflate(R.layout.dialogx, null);
+            ad = new AlertDialog.Builder(this);
+            ad.setCancelable(false);
+            ad.setTitle("Confirm deleting value from Firebase");
+            ad.setView(dialog);
+            ad.setPositiveButton("confirm", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    String str = ("l" + position);
+                    if ((!phonestudent1.equals("08:00-08:40")) && !phonestudent1.equals("08:45-09:25") && !phonestudent1.equals("10:00-10:40") && !phonestudent1.equals("10:45-11:25") && !phonestudent1.equals("12:30-13:10") && !phonestudent1.equals("13:15-13:55") &&
+                            !phonestudent1.equals("14:00-14:40") && !phonestudent1.equals("14:45-15:25") && !phonestudent1.equals("19:00-19:40") && !phonestudent1.equals("19:45-20:25")) {
+
+                        for (int x = 0; x <= 9; x++)
+                            phonestudent = phonestudent + phonestudent1.charAt(x);
+                        refStudent.child(phonestudent).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                student.copyStudent(dataSnapshot.getValue(Ustudents.class));
+                                count1 = student.getCount();
+                                t = Integer.parseInt(count1);
+                                t--;
+                                String sr = "count";
+                                String sc = Integer.toString(t);
+                                refStudent.child(phonestudent).child(sr).removeValue();
+                                refStudent.child(phonestudent).child(sr).setValue(sc);
+                                Toast.makeText(TeacherLessons.this, "Deleting succeeded", Toast.LENGTH_SHORT).show();
+                                phonestudent = "";
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+                    }
                     refTeacherTime.child(phone1).child(day).child(str).removeValue();
                     refTeacherTime.child(phone1).child(day).child(str).setValue("Canceled");
-
-
-
                     //  count1++;
-                //refStudent.child(phonestudent).child("count").setValue(count1);
-                dialogInterface.dismiss();
-            }
-        });
-        ad.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-        AlertDialog adb = ad.create();
-        adb.show();
+                    //refStudent.child(phonestudent).child("count").setValue(count1);
+                    dialogInterface.dismiss();
+                }
+            });
+            ad.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            AlertDialog adb2 = ad.create();
+            adb2.show();
 
+        }
     }
     public boolean onCreateOptionsMenu (Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
